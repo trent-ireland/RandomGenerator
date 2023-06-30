66be068f5006a769a85ac3f8d582d7db329a4add
Trent Ireland <trentireland87@gmail.com>
2023-06-30 15:35:27 +0100

    more added

--- code.js
+++ code.js
@@ -1,8 +1,7 @@
 //array for randomword of Affirmation
 const quoteStart = ["I am", "Today", "My" ];
-const quoteEnd1 = ["Strong", "Smart", "Sucessful", "Sexy", "Funny"];
+const quoteEnd1 = ["Strong", "Smart", "Sucessful", "Sexy", "Funny","Powerful",];
 const quoteEnd2 = ["is a phenomenal day", "will be a productive day",]
 //chooses random Index for quotestart
 const index1 = Math.floor(Math.random() * quoteStart.length);
 const index2 = Math.floor(Math.random() * positiveWords.length);
-console.log(index1);
